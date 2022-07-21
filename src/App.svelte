2bd64996cedeb01db55fb3372f0887cfbf641172
Antonio Medina Conejero <95857076+anmeco@users.noreply.github.com>
2022-07-21 12:36:55 +0200

    First Commit
    Template Map

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import MapView from "@arcgis/core/views/MapView";
+  import "@arcgis/core/assets/esri/themes/light/main.css";
+
+  let centerText;
+  const createMap = (domNode) => {
+    const view = new MapView({
+      container: domNode,
+      map: {
+        basemap: "oceans",
+        ground: "world-topobathymetry",
+        
+      },
+      zoom: 5,
+      center: [-5.97317, 37.38283],
+    });
+    view.watch("center", (center) => {
+      const {latitude, longitude} = center;
+      centerText = `Posicion: Latitud: ${latitude.toFixed(4)} | Longitud: ${longitude.toFixed(4)}`;
+  })
+
+};
+</script>
+
+<main>
+  <div class="view" use:createMap></div>
+  {#if centerText}
+  <p>{centerText}</p>
+  {/if}
+</main>
+
+<style>
+    .view {
+    height: 800px;
+    width: 1200px;
+  }
+</style>
